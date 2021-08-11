f6676b6025ef56e1d248d0d909b864ad73c70261
vntuan13579 <64194723+vntuan13579@users.noreply.github.com>
2021-08-11 16:12:01 +0700

    add some function on search.cpp

--- searchEngine/searchEngine/search.cpp
+++ searchEngine/searchEngine/search.cpp
@@ -1,42 +1,134 @@
 #include "search.h"
 
+void preprocess_exactSearch(string& input)
+{
+    int Aa = 'A' - 'a';
+    string tmp = "";
+    for (int i = 0; i < input.size(); ++i)
+    {
+        if (input[i] <= 'Z' && input[i] >= 'A')
+        {
+            input[i] -= Aa;
+        }
+    }
+
+    // i am so lazy
+    for (int i = 0; i < input.size(); ++i)
+    {
+        if (input[i] > 'z' && input[i] < 'a' && input[i] < '0' && input[i] > '9' && input[i] != '#' && input[i] != '$' && input[i] != '*')
+        {
+            continue;
+        }
+        tmp += input[i];
+    }
+
+    input = tmp;
+}
+
 void preprocess(string& input)
 {
     int Aa = 'A' - 'a';
-
+    string tmp = "";
     for (int i = 0; i < input.size(); ++i)
     {
-
+        if (input[i] <= 'Z' && input[i] >= 'A')
+        {
+            input[i] -= Aa;
+        }
     }
 
+    // i am so lazy
     for (int i = 0; i < input.size(); ++i)
     {
         if (input[i] > 'z' && input[i] < 'a' && input[i] < '0' && input[i] > '9' && input[i] != '#' && input[i] != '$' && input[i] != '*' 
-            && input[i] != '"')
+            && input[i] != '"'  && input[i] != '-')
         {
-
+            continue;
         }
+        tmp += input[i];
     }
+
+    input = tmp;
 }
+
+vector<string> stringToWord(const string& input)
+{
+    vector<string> result;
+    string tmp;
+
+    for (int i = 0; i < input.size(); ++i)
+    {
+        if (input[i] == ' ')
+        {
+            result.push_back(tmp);
+            tmp = "";
+        }
+        else
+            tmp += input[i];
+    }
+
+    return result;
+}
+
+// 
+// 
 
 vector<searchData> search(const vector<fileData>& docData, const vector<fileData>& stopwordData,
     const vector<fileData>& synonymData, const string& query)
 {
+    vector<searchData>  result;
+
 	//cut the query
+    vector<string> cutQuery = stringToWord(query);
 
-    //preprocess query
+    //exact search (and wild card I guess...)
+    string firstWord = cutQuery[0];
+    string lastWord = cutQuery[cutQuery.size() - 1];
+    if (firstWord[0] == '"' && lastWord[lastWord.size() - 1] == '"')
+    {
 
-    //remove stopwords 
-
-	vector<searchData>  result;
-
-    //exact search (and wild card)
- 
+    }
     //intitle search
 
     // AND *
 
     // OR *
+
+    if (cutQuery.size() == 3)
+    {
+        if (cutQuery[1] == "AND")
+        {
+            preprocess(cutQuery[0]);
+            preprocess(cutQuery[2]);
+            //call AND search
+        }
+        else if (cutQuery[1] == "OR")
+        {
+            preprocess(cutQuery[0]);
+            preprocess(cutQuery[2]);
+            //call OR search
+        }
+    }
+
+
+
+    //preprocess query
+    for (int i = 0; i < cutQuery.size(); ++i)
+    {
+        preprocess(cutQuery[i]);
+    }
+
+    //remove stopwords (wait for trie....)
+
+
+    // start the search
+	
+
+    
+ 
+
+
+    //NOT *
 
     //search price range *
 
@@ -79,10 +171,21 @@ vector<int> normalSearch(const fileData& file, const string& key)
     return (pCrawl->place);
 }
 
-vector<int> exactSearch(const fileData& file, const vector<string>& query)
+vector<int> exactSearch(const fileData& file, vector<string>& query)
 {
     vector<int> empty;
-    
+    vector<int> firstWordLocation = normalSearch(file, query[0]);
+
+    for (int i = 0; i < query.size(); ++i)
+    {
+        if (query[i] == "*")
+            continue;
+
+        if (normalSearch(file, query[i]).size() == 0)
+            return empty;
+    }
+
+    //for (int i )
     return empty;
 }
 
--- searchEngine/searchEngine/search.h
+++ searchEngine/searchEngine/search.h
@@ -16,14 +16,14 @@ struct searchData
 vector<searchData> search(const vector<fileData>& docData, const vector<fileData>& stopwordData,
 	const vector<fileData>& synonymData, const string& query);
 
+vector<int> exactSearch(const fileData& file, vector<string>& query);
+
 // done
 vector<int> normalSearch(const fileData& file, const string& key);
-
-
-vector<int> exactSearch(const fileData& file,const vector<string>& query); // not done
-
-//vector<string> stringToWord(string input);
-
+vector<string> stringToWord(const string & input);
 void preprocess(string& input);
+void preprocess_exactSearch(string& input);
+
+
 
 //void testDisplay(const string &fileName,  const vector<int> &places);
