2c384b44e87611c9633c790529a460bac720928c
vntuan13579 <64194723+vntuan13579@users.noreply.github.com>
2021-08-14 16:06:52 +0700

    finish exact search, chooseTop5

--- searchEngine/searchEngine/search.h
+++ searchEngine/searchEngine/search.h
@@ -2,6 +2,7 @@
 #ifndef _SEARCH_H_
 #define _SEARCH_H_
 #include "Trie.h"
+#include <iostream>
 #include <fstream>
 #include<string>
 using namespace std;
@@ -20,15 +21,19 @@ struct searchData
 vector<searchData> search(const vector<fileData>& docData, const vector<fileData>& stopwordData,
 	const vector<fileData>& synonymData, const string& query);
 
-vector<int> exactSearch(const fileData& file, vector<string>& query);
+vector<searchData> selectTop5(vector<searchData>& searchResult);
 
 // done
 vector<int> normalSearch(const fileData& file, const string& key);
 vector<string> stringToWord(const string & input);
 void preprocess(string& input);
 void preprocess_exactSearch(string& input);
+vector<int> exactSearch(const fileData& file, vector<string>& query);
 
-
+//sorting
+int getMax(vector<int> &arr);
+void countSort(vector<int>& arr, int exp);
+void radixsort(vector<int>& arr);
 
 //void testDisplay(const string &fileName,  const vector<int> &places);
 
