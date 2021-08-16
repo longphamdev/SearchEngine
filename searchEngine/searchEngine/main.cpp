86b20e44181c47ec8c9ae657afcc6a192d505d8d
nhson19 <nhson19@apcs.vn>
2021-08-16 15:29:07 +0700

    Update main.cpp

--- searchEngine/searchEngine/main.cpp
+++ searchEngine/searchEngine/main.cpp
@@ -5,116 +5,55 @@
 
 int main()
 {
-	//ifstream fin;
-	//string query;
-	//const vector<fileData> docData;
-	//const vector<fileData> stopwordData;
-	//const vector<fileData> synonymData;
-	//vector<searchData> result;
-	//searchData tmp;
-	//tmp.fileName = "1.txt";
-	//tmp.place.push_back(10);
-	//result.push_back(tmp);
-	//tmp.fileName = "000.txt";
-	//result.push_back(tmp);
-
-	//DrawMain();
-	//GotoXY(21, 15);
-	//getline(cin, query);
-	//Sleep(500);
-	//system("CLS");
-	//	
-	////result = search(docData, stopwordData, synonymData,  query);
-	//
-
-
-	//for (int i = 0; i < 2; i++) {
-	//	TextColor(241);
-	//	cout << "[ " << i+1 << " ]";
-	//	cout << result[i].fileName << endl;
-	//	TextColor(240);
-	//	string temp;
-	//	fin.open("C:/Users/tin85/OneDrive/M�y t�nh/GitHub/SearchEngine/searchEngine/searchEngine/Search Engine-Data/Search Engine-Data/" + result[i].fileName);
-	//	if (!fin.is_open()) {
-	//		cout << "Can not open file";
-	//	}
-	//	else {
-	//		for (int j = 0; j < 3; j++) {
-	//			getline(fin, temp);
-	//			cout << temp;
-	//		}
-	//		cout << endl;
-	//		cout << "--------------------------------------------------------- " << endl;
-	//	}
-	//	fin.close();
-	//}
-
-	//int choose;
-	//cout << "Select chosen file: ";
-	//cin >> choose;
-	//Sleep(500);
-	//system("CLS");
-
-	//string temp;
-	//fin.open("C:/Users/tin85/OneDrive/M�y t�nh/GitHub/SearchEngine/searchEngine/searchEngine/Search Engine-Data/Search Engine-Data/" + result[choose -1].fileName);
-	//if (!fin.is_open()) {
-	//	cout << "Can not open file";
-	//}
-	//else {
-	//	while (!fin.eof()) {
-	//		getline(fin, temp);
-	//		cout << temp;
-	//	}
-	//}
-	//fin.close();
-
-	vector<fileData> docData = getFileData();
-	fileData stopWord = getStopWord();
+	ifstream fin;
 	string query;
-	cout << "enter the query: ";
-	cin >> query;
-	vector<searchData> result = search(docData, stopWord, query);
-	for (int i = 0; i < result.size(); ++i)
-		displayTest(result[i]);
+	const vector<fileData> docData;
+	const vector<fileData> stopwordData;
+	const vector<fileData> synonymData;
+	vector<searchData> result;
+	searchData tmp;
+	tmp.fileName = "1.txt";
+	tmp.place.push_back(10);
+	result.push_back(tmp);
+	tmp.fileName = "000.txt";
+	result.push_back(tmp);
 
-	cin.ignore(1000, '\n');
-	cout << endl << "press enter to continue...";
-	cin.get();
-		////vector<fileData> test;
-		////
-		////test.push_back(tmp);
-	//DrawMain();
+	DrawMain();
+	GotoXY(21, 15);
+	getline(cin, query);
+	Sleep(500);
+	system("CLS");
+		
+	char status = 'n';
+	//result = search(docData, stopwordData, synonymData,  query);
 
-	//GotoXY(21, 15);
-	//getline(cin, query);
-	//Sleep(500);
-	//system("CLS");
+	while (status != 'x'){
 
-	//char status = 'y';
-	////result = search(docData, stopwordData, synonymData,  query);
+		displaySearchResult(fin, result);
 
-	//while (status != 'x'){
-	//	displaySearchResult(fin, result);
+		cout << endl;
+		displayOption(status);
 
-	//	cout << endl;
-	//	displayOption(status);
-
-	//    if (status == 'x') {
-	//		exit(1);
-	//    }
-	//    if (status == 'y') {
-	//		system("CLS");
-	//	    DrawMain();
-	//		GotoXY(21, 15);
-	//		cin.ignore();
-	//		getline(cin, query);
-	//		Sleep(500);
-	//		system("CLS");
-	//    }
-	//	if (status == 'n') {
-	//		system("CLS");
-	//	}
- //   }
+		switch (status) {
+		case 'x':
+			exit(1);
+			break;
+		case 'y':
+			system("CLS");
+			DrawMain();
+			GotoXY(21, 15);
+			cin.ignore();
+			getline(cin, query);
+			Sleep(500);
+			system("CLS");
+			break;
+		case 'n':
+			system("CLS");
+			break;
+		default:
+			break;
+		}
+    }
 
 
 	return 0;
